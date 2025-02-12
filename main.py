cc277a0c79c6e95b0831b3c08e481d12a7fea96a
stradoc <weien.greathouse@gmail.com>
2025-02-11 19:36:59 -0600

    feat: set up data scaling
    * set up data scaling for the randomly generated dataset
    * fixed the random.seed() to hold a value

--- main.py
+++ main.py
@@ -14,4 +14,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # set seed for reproducibility
-np.random.seed()
+np.random.seed(0)
+
+# generate data points
+original_data = np.random.exponential(size=1000)
+
+#min-max scale the data between 0 and 1
+scaled_data = minmax_scaling(original_data, columns=0)
+
+# plot everything
+fig, ax = plt.subplots(1, 2, figsize=(15,3))
+sns.histplot(original_data, ax=ax[0], kde=True, legend=False)
+ax[0].set_title("Original Data")
+sns.histplot(scaled_data, ax=ax[1], kde=True, legend=False)
+ax[1].set_title("Scaled Data")
+plt.show()
+
